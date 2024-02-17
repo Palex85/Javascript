f678449d4b4bdcf75dac31a5a555a11a78de8191
Palex85 <pauloajn1@hotmail.com>
2024-02-17 17:54:39 -0300

    first commit

---
+++ Projeto.js/scripts.js
@@ -0,0 +1,18 @@
+const convertButton = document.querySelector(".convert-button")
+
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //valor em real
+    const currencyValueConverted = document.querySelector(".currency-value") //outras moedas
+    const dollarToday = 5.2
+
+    const convertedValue = inputCurrencyValue / dollarToday
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+      style: "currency", currency:"BLR"}).format(inputCurrencyValue)
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"})
+    .format(convertedValue)
+
+  console.log(convertedValue)
+}
+
+ convertButton.addEventListener("click", convertValues)
